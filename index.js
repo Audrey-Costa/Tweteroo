fcfab1897606c0db66e340044eab2b66fdf29623
Audrey-Costa <audreycosta@nano.ufrj.br>
2022-06-20 10:10:00 -0300

    Post /Sign-up

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from "express";
-import axios from "axios";
 import cors from "cors"
+import fs from "fs"
 
 const server = express();
 server.use(cors());
+server.use(express.json());
 
+let users = JSON.parse(fs.readFileSync("usersDatabase.json", "utf-8"));
 
+server.post("/sign-up", (req, res)=>{
+    const {username, avatar} = req.body;
+    if(!username||!avatar){
+        res.status(400).send("Name or Avatar required!");
+        return;
+    }
+    users.user.push({
+        username,
+        avatar
+    })
 
-server.listen(5000);
+    fs.writeFileSync("usersDatabase.json", JSON.stringify(users, null, 2));
+    res.status(200).send("OK")
+})
+
+server.listen(5000, ()=>{
+    console.log("Servidor Acessado")});
